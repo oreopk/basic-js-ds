82c4b3fcbb43097bb90dabc63bedc1cdd4778986
oreopk <sart6zaV@yandex.ru>
2025-11-09 21:16:45 +0300

    feat: done 50% tasks in binary-search-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,33 +1,74 @@
 const { NotImplementedError } = require('../lib/errors');
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor() {
+    this.rootTree = null;
+  }
   root() {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+    return this.rootTree;
   }
 
-  add(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  add(data) {
+    const newNode = new Node(data);
+    if(this.rootTree===null){
+      this.rootTree=newNode;
+      return;
+    }
+    let current = this.rootTree;
+    while (true) {
+      if(newNode.data < current.data){
+        if (current.left === null) {
+          current.left = newNode; 
+          return;
+        } else {
+          current= current.left;
+        }
+      } else {
+          if (current.right  === null) {
+            current.right  = newNode; 
+            return;
+          } else {
+            current= current.right;
+          }
+      }
+    }
   }
 
-  find(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
+  find(data) {
+    let current = this.rootTree;
+      while (current !== null) {
+        if(data == current.data){
+          return current;
+        } else if (data < current.data) {
+          current = current.left;
+        } else {
+          current = current.right;
+        }
+      }
+      return null;
+    }
+
+
+  has(data) {
+      let current = this.rootTree;
+      while (current !== null) {
+        if(data == current.data){
+          return true;
+        } else if (data < current.data) {
+          current = current.left;
+        } else {
+          current = current.right;
+        }
+      }
+      return false;
   }
 
-  has(/* data */) {
-    // Remove line below and write your code here
-    throw new NotImplementedError('Not implemented');
-  }
-
-  remove(/* data */) {
-    // Remove line below and write your code here
+  remove(data) {
     throw new NotImplementedError('Not implemented');
   }
 
